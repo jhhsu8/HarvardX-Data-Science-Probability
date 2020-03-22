e1b2be2a72d86536eeaeec7c9e8c12146cb1516c
jhhsu8 <jhhsu11@gmail.com>
2020-03-21 23:12:20 -0700

    Update addition_rule.R

--- addition_rule.R
+++ addition_rule.R
@@ -1,50 +1,37 @@
 #The Addition Rule
 
 #1. The Cavs and the Warriors
-
 # Assign a variable 'n' as the number of remaining games.
 n <- 6
-
 # Assign a variable `outcomes` as a vector of possible game outcomes, where 0 indicates a loss and 1 indicates a win for the Cavs.
 outcomes <- c(1,0)
-
 # Assign a variable `l` to a list of all possible outcomes in all remaining games. Use the `rep` function on `list(outcomes)` to create list of length `n`.
 l <- rep(list(outcomes),n)
-
 # Create a data frame named 'possibilities' that contains all combinations of possible outcomes for the remaining games.
 possibilities <- expand.grid(l)
 possibilities 
-
 # Create a vector named 'results' that indicates whether each row in the data frame 'possibilities' contains enough wins for the Cavs to win the series
 results <- rowSums(possibilities) >= 4
 results
-
 # Calculate the proportion of 'results' in which the Cavs win the series. Print the outcome to the console.
 mean(results)
 
 #2. The Cavs and the Warriors - Monte Carlo
-
 # The variable `B` specifies the number of times we want the simulation to run. Let's run the Monte Carlo simulation 10,000 times.
 B <- 10000
-
 # Use the `set.seed` function to make sure your answer matches the expected result after random sampling.
 set.seed(1)
-
 # Create an object called `results` that replicates for `B` iterations a simulated series and determines whether that series contains at least four wins for the Cavs.
 results <- replicate(B, {
   simulated_series <- sample(c(0,1), 6, replace = TRUE)
   sum(simulated_series) >= 4
 })
-
 # Calculate the frequency out of `B` iterations that the Cavs won at least four games in the remainder of the series. Print your answer to the console.
-
 mean(results)
 
 #3. A and B play a series - part 1
-
 # Let's assign the variable 'p' as the vector of probabilities that team A will win.
 p <- seq(0.5, 0.95, 0.025)
-
 # Given a value 'p', the probability of winning the series for the underdog team B can be computed with the following function based on a Monte Carlo simulation:
 prob_win <- function(p){
   B <- 10000
@@ -54,17 +41,13 @@ prob_win <- function(p){
     })
   mean(result)
 }
-
 # Apply the 'prob_win' function across the vector of probabilities that team A will win to determine the probability that team B will win. Call this object 'Pr'.
 Pr<-sapply(p, prob_win)
 Pr
-
 # Plot the probability 'p' on the x-axis and 'Pr' on the y-axis.
 plot(p, Pr)
 
-
 #4. A and B play a series - part 2
-
 # Given a value 'p', the probability of winning the series for the underdog team B can be computed with the following function based on a Monte Carlo simulation:
 prob_win <- function(N, p=0.75){
       B <- 10000
@@ -74,13 +57,10 @@ prob_win <- function(N, p=0.75){
         })
       mean(result)
     }
-
 # Assign the variable 'N' as the vector of series lengths. Use only odd numbers ranging from 1 to 25 games.
 N <- seq(1, 25, 2)
 N
-
 # Apply the 'prob_win' function across the vector of series lengths to determine the probability that team B will win. Call this object `Pr`.
 Pr <- sapply(N, prob_win)
-
 # Plot the number of games in the series 'N' on the x-axis and 'Pr' on the y-axis.
 plot(N, Pr)
